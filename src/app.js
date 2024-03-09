65cb8c53344e7c5c8ddf5dadb7946239a6647351
Net Stark <105382120+Netodev3@users.noreply.github.com>
2024-03-09 01:56:36 +0000

    Acabado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Guardamos los selectores en variables
+  const cardValue = document.querySelector(".number");
+  const topCardSuit = document.querySelector(".top-suit");
+  const bottomCardSuit = document.querySelector(".bottom-suit");
+
+  // Valores posibles de la carta
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  // Generamos valores aleatorios
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  // Generar palo aleatorio para la carta (♠, ♣, ♥, ♦)
+  const suits = ["♠", "♣", "♥", "♦"]; // Espadas, Tréboles, Corazones, Diamantes
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  // Asignamos un valor aleatorio
+  cardValue.innerHTML = randomValue;
+
+  // Asignamos un palo aleatorio
+  topCardSuit.innerHTML = randomSuit;
+  bottomCardSuit.innerHTML = randomSuit;
+
+  // Aplicamos el color rojo o negro en función del palo
+  if (randomSuit === "♥" || randomSuit === "♦") {
+    topCardSuit.classList.add("red");
+    bottomCardSuit.classList.add("red");
+    cardValue.classList.add("red");
+  } else {
+    topCardSuit.classList.add("black");
+    bottomCardSuit.classList.add("black");
+    cardValue.classList.add("black");
+  }
 };
